beaef81477bca0deabd8b33dd5f5ebc84c4fb0be
marcopestrin <pestrinmarco@gmail.com>
2025-08-22 14:13:58 +0200

    try to propagate modify

--- packages/blog-core/src/index.ts
+++ packages/blog-core/src/index.ts
@@ -2,6 +2,5 @@ export * from "./db";
 export * as Tenants from "./services/tenants";
 export * as Articles from "./services/articles";
 export * as Tags from "./services/tags";
-export * from "./types";
 export * as Authors from "./services/authors";
-
+export * from "./types";
